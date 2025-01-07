b053b4d2c05224847ee0a8d8de84ae3a6fe8f844
ksh4450 <shkang4450@gmail.com>
2025-01-07 16:45:00 +0900

    Create 2884.java

---
+++ Baekjoon/java/2884.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int h = sc.nextInt();
+        int m = sc.nextInt();
+
+        if(m-45 < 0){
+            if(h-1 < 0){
+                System.out.print("23" + " " + (60+(m-45)));
+            }else{
+                System.out.println(h-1 + " " + (60+(m-45)));
+            }
+        }else{
+            System.out.println(h + " " + (m-45));
+        }
+    }
+}
