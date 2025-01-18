267221e917983cd43ee4bdab3cdf490e4e0294c6
ksh4450 <shkang4450@gmail.com>
2025-01-18 09:51:55 +0900

    Create 15552.java

--- Baekjoon/java/15552.java
+++ Baekjoon/java/15552.java
@@ -4,7 +4,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 import java.io.IOException;
-
+ 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
